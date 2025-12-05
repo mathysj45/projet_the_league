f68028cf4f3ec8a27654e69ba20600f1a385a4fe
mathys <mathys.j45@gmail.com>
2025-12-05 15:46:55 +0100

    modif

--- managers/AbstractManager.php
+++ managers/AbstractManager.php
@@ -7,7 +7,7 @@ abstract class AbstractManager
     {
         $host = "localhost";
         $port = "3306";
-        $dbname = "coda_manager";
+        $dbname = "the_league";
         $connexionString = "mysql:host=$host;port=$port;dbname=$dbname;charset=utf8";
 
         $user = "root";
