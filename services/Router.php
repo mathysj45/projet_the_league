5cc689b930ad62390d3e33090742528cd230e6e2
mathys <mathys.j45@gmail.com>
2025-12-05 10:47:23 +0100

    Ajout de fichier nécessaire au router

--- services/Router.php
+++ services/Router.php
@@ -1,10 +1,40 @@
 <?php
-
-class Router
-{
-    public function handleRequest() : void
+    class Router
     {
-        $ctrl = new DefaultController();
-        $ctrl->index();
+        public function handleRequest(array $get) : void
+        {
+            if(isset($_GET["route"]))
+            {
+                if($_GET["route"] === "team")
+                {
+                    $ctrl = new PageController();
+                    $ctrl->team();
+                }
+
+                else if ($_GET["route"] === "player")
+                {
+                    $ctrl = new PageController();
+                    $ctrl->player();
+                }
+
+                else if ($_GET["route"] === "match")
+                {
+                    $ctrl = new PageController();
+                    $ctrl->match();
+                }
+
+                else
+                {
+                    $ctrl = new PageController();
+                    $ctrl->notFound();
+                }
+            }
+
+            else
+            {
+                $ctrl = new PageController();
+                $ctrl->home();
+            }
+        }
     }
-}
+?>
