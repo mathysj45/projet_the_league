ab3cfaf40e3bc165d7f234f98d8d7fd58ebf8284
mathys <mathys.j45@gmail.com>
2025-12-05 16:47:32 +0100

    modif

--- controllers/AbstractController.php
+++ controllers/AbstractController.php
@@ -4,8 +4,8 @@ abstract class AbstractController
 {
     protected function render(string $template, array $data) : void
     {
-        var_dump($data);
-        die;
+        // var_dump($data);
+        // die;
         require "templates/layout.phtml";
     }
 
