6e26f40f567dd0082c792f5f35aee7223bf66bb9
Tsuki <alban.elie590@gmail.com>
2025-12-05 10:09:13 +0100

    connection de la base de nonnee

---
+++ config/database.php
@@ -0,0 +1,15 @@
+<?php 
+$host = "localhost";
+$port = "3306";
+$dbname = "the_league";
+$connexionString = "mysql:host=$host;port=$port;dbname=$dbname;charset=utf8";
+
+$user = "root";
+$password = "";
+
+$db = new PDO(
+    $connexionString,
+    $user,
+    $password
+);
+
