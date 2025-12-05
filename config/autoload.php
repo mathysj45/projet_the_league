6c8ec7da366259f08ac84b94e1a41bcc44764da1
Tsuki <alban.elie590@gmail.com>
2025-12-05 15:49:39 +0100

    rien

--- config/autoload.php
+++ config/autoload.php
@@ -5,7 +5,7 @@ require "models/Players.php";
 require "managers/AbstractManager.php";
 require "managers/TeamManager.php";
 require "managers/PlayerManager.php";
-require "models/Game.php";        
+require "models/Games.php";        
 require "managers/GameManager.php"; 
 
 require "controllers/AbstractController.php";
--- controllers/PageController.php
+++ controllers/PageController.php
@@ -5,10 +5,9 @@
 
 class PageController extends AbstractController 
 {
-    --- ACCUEIL ---
+    
     public function home() : void
     {
-<<<<<<< HEAD
     //     $teamManager = new TeamManager();
     //     $playerManager = new PlayerManager();
     //     $matchManager = new MatchManager();
@@ -26,22 +25,6 @@ class PageController extends AbstractController
 
         $this->render("home", ["pageTitle" => "The league"]);
 
-=======
-        $teamManager = new TeamManager();
-        $playerManager = new PlayerManager();
-        $gameManager = new GameManager();
-
-        $teams = $teamManager->getAllTeam();
-        $players = $playerManager->getAllPlayers();
-        $games = $gameManager->getAllGames();
-
-        $this->render("home", [
-            "pageTitle" => "The League",
-            "teams" => $teams,
-            "players" => $players,
-            "matches" => $games
-        ]);
->>>>>>> df63f18776626a88a7dd781b814d9e31db2e080b
 
     }
 
