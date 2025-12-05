0f3c0d2ae9a2310e893e6d168ae991238e7ff916
Tsuki <alban.elie590@gmail.com>
2025-12-05 11:09:32 +0100

    team manager

--- index.php
+++ index.php
@@ -3,4 +3,7 @@
 require "config/autoload.php";
 
 $router = new Router();
-$router->handleRequest();
+$router->handleRequest($_GET);
+
+$truc = new Team(1,"bob","un tuc","jcp");
+echo $truc->getName();
