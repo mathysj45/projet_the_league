537f1a5c070ab70cfbac14758c2dc282e6196244
Tsuki <alban.elie590@gmail.com>
2025-12-05 10:38:22 +0100

    abstracmanager et team

---
+++ managers/AbstractManager.php
@@ -0,0 +1,22 @@
+<?php
+abstract class AbstractManager
+{
+    protected PDO $db;
+
+    public function __construct()
+    {
+        $host = "localhost";
+        $port = "3306";
+        $dbname = "the_league";
+        $connexionString = "mysql:host=$host;port=$port;dbname=$dbname;charset=utf8";
+
+        $user = "root";
+        $password = "";
+
+        $this->db = new PDO(
+            $connexionString,
+            $user,
+            $password
+        );
+    }
+}
