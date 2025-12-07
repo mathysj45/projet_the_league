d91699e577acbd36336afd40b032206b3aeb6ef5
Tsuki <alban.elie590@gmail.com>
2025-12-07 13:43:50 +0100

    rien

--- managers/Player_PerformanceManager.php
+++ managers/Player_PerformanceManager.php
@@ -7,7 +7,6 @@ class Player_PerformanceManager extends AbstractManager
         parent::__construct();
     }
 
-    // On précise que la fonction retourne un tableau d'objets PlayerPerformance
     public function getStatsByPlayerId(int $id): array
     {
         $query = $this->db->prepare("SELECT player, game, points, assists FROM player_performance WHERE player = :id");
@@ -16,12 +15,10 @@ class Player_PerformanceManager extends AbstractManager
         ];
         $query->execute($parameters);
         
-        // On récupère les résultats en tableau associatif
         $results = $query->fetchAll(PDO::FETCH_ASSOC);
         
         $performances = [];
 
-        // On boucle sur chaque résultat pour créer un objet
         foreach ($results as $data) {
             $performance = new PlayerPerformance(
                 (int)$data['player'],
@@ -35,9 +32,6 @@ class Player_PerformanceManager extends AbstractManager
         
         return $performances;
     }
-
-    // Tu devras faire la même logique pour getPerformance() si tu l'utilises
-    // Récupère TOUTES les performances de la base de données
     public function getPerformance(): array
     {
         $query = $this->db->prepare("SELECT player, game, points, assists FROM player_performance");
