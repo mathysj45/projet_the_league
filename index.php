291d9fab6ec3f4bcb1cffdc6a3f16c1937d939ea
Tsuki <alban.elie590@gmail.com>
2025-12-07 17:54:45 +0100

    eee

--- index.php
+++ index.php
@@ -1,10 +1,13 @@
+
 <?php
 
-require "config/autoload.php";
-$router = new Router();
-$router->handleRequest($_GET);
-$TeamManager = new TeamManager();
+require "config/autoload.php"; 
 
 define('BASE_URL', '/cour-php-coda/projet_the_league'); // <-- A adapter par chaque développeur !
-require 'controllers/AbstractController.php';
+
+$router = new Router();
+$router->handleRequest($_GET);
+
+$TeamManager = new TeamManager(); 
+
 ?>
