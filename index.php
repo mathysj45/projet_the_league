4e36e6ecae77bcf54d8615de566ae93421f09fca
mathysj45 <mathys.j45@gmail.com>
2025-12-07 19:43:08 +0100

    Bouton back supp

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 
 <?php
 
-<<<<<<< HEAD
 require "config/autoload.php";
 define('BASE_URL', '/projet_the_league'); // <-- A adapter par chaque développeur !
 $router = new Router();
@@ -9,15 +8,4 @@ $router->handleRequest($_GET);
 $TeamManager = new TeamManager();
 
 require 'controllers/AbstractController.php';
-=======
-require "config/autoload.php"; 
-
-define('BASE_URL', '/cour-php-coda/projet_the_league');
-
-$router = new Router();
-$router->handleRequest($_GET);
-
-$TeamManager = new TeamManager(); 
-
->>>>>>> 291d9fab6ec3f4bcb1cffdc6a3f16c1937d939ea
 ?>
